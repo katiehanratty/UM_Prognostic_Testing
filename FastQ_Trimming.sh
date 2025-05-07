7ed602184d12daef544c805897ca6c2c6c38305d
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-07 15:03:43 +0100

    Update FastQ_Trimming.sh

--- FastQ_Trimming.sh
+++ FastQ_Trimming.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#TRIMMING FASTQ FILES
 #set working directory to users fastq directory
+cd ~/fastq
+
 #run cutadapt 
 for i in *_1.fastq.gz; do
-cutadapt -j=1 -a AGATCGGAAGAG -A AGATCGGAAGAG \  #change sequence as needed
+cutadapt -j=1 -a AGATCGGAAGAG -A AGATCGGAAGAG \  #change sequence as needed (This is Illumina adapter)
 --output=trimmed_$i --paired-output=trimmed_${i%_1.fastq.gz}_2.fastq.gz \
---error-rate=0.1 --times=1 --overlap=3 --action=trim --minimum-length=35 \
---pair-filter=any --quality-cutoff=20 \
-$i ${i%_1.fastq.gz}_2.fastq.gz > trimmed_${i%_1.fastq.gz}_cutadapt.log
+--error-rate=0.1 --times=1 --overlap=3 --action=trim --minimum-length=35 \ 
+--pair-filter=any --quality-cutoff=20 \ 
+$i ${i%_1.fastq.gz}_2.fastq.gz > trimmed_${i%_1.fastq.gz}_cutadapt.log   #adjust other options as required
 done
 
 #move trimmed files to their own directory 
-mkdir -p ~/trimmed_fastq/FastQC
+mkdir -p ~/trimmed_fastq/FastQC/MultiQC
 mv trimmed*fastq.gz ~/trimmed_fastq
 
 #run fastqc and multiqc on the trimmed files
-fastqc ~/trimmed_fastq/*.fastq.gz 
-multiqc ~/trimmed_fastq --outdir=~/trimmed_fastq/FastQC
+cd ~/trimmed_fastq
+fastqc *.fastq.gz 
+multiqc ~/trimmed_fastq --outdir=~/trimmed_fastq/FastQC/MultiQC
+
+#move FASTQC files to their own directory
+mv *fastqc* FastQC
 
