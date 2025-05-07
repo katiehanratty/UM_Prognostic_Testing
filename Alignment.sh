b049ead4ac8f8dffe25d4fa38bcc3e77c8a8cb09
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-07 13:58:19 +0100

    Update Alignment.sh

--- Alignment.sh
+++ Alignment.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
-#make directory for alignment 
+#MAKE DIRECTORY FOR ALIGNMENT
 mkdir -p ~/alignment/bamfiles
 mkdir -p ~/alignment/samfiles
 
-#ALIGN FASTQ TO HG38 REFERENCE 
+#DOWNLOAD REFERENCE SEQUENCE OF CHOICE TO REFERENCE DIRECTORY
+mkdir ~/alignment/reference
+wget #add in link to ftp server's reference wanted 
 
+#ALIGN FASTQ TO REFERENCE 
+
+#MOVE INTO DIRECTORY WITH FASTQ FILES
+cd ~/fastq 
+
+#CREATE LOOP
 for i in *_R1.fastq.gz; do
-bwa mem -t 8 ~/alignment/reference/hg38.fa $i \  #edit threads as needed
+bwa mem -t 8 ~/alignment/reference/hg38.fa $i \  #edit threads (-t) as needed
 ${i%_R1.fastq.gz}_R2.fastq.gz > ${i%_R1.fastq.gz}.sam
 
 #SAM TO BAM, SORTING AND INDEXING BAM FILE
-
 samtools view -b ${i%_R1.fastq.gz}.sam | samtools sort -o ${i%_R1.fastq.gz}_sorted.bam
 samtools index ${i%_R1.fastq.gz}_sorted.bam
 done 
 
-#move files to bamfile directory
+#MOVE FILES TO BAMFILE/SAMFILE DIRECTORY
 mv *.bam ~/alignment/bamfiles
 mv *.sam ~/alignment/samfiles
 
