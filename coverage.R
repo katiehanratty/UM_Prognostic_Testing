2a0cbdac2405f9bec9000a5bbb474000b680a290
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-07 15:35:32 +0100

    Update coverage.R

--- coverage.R
+++ coverage.R
@@ -5,27 +5,29 @@
 install.packages(dplyr)
 library(dplyr)
 
+#set working directory to where bedtools coverage files are. 
 
-
+#list the depth files in working directory.
 bed_files <- list.files(pattern = "*.txt") 
+
 get_value <- function(df, depth_value, column) {
   value <- df %>% filter(depth == depth_value) %>% slice_head(n = 1) %>% pull({{ column }})
   if (length(value) == 0) return(NA) else return(value)
 }
 
-#create loop for all pileup files 
+#create loop for all depth of coverage files
 
-# Loop through each file
+# Loop through each file and create a data table from all rows with 'all' in column 1
 for (file in bed_files) {
   data <- read.table(file=file, header=FALSE)
   colnames(data) <- c("col1", "depth", "num_bases", "total_targeted_bases", "proportion_targeted_bases")
   coverage <- filter(data, col1 == "all") %>% arrange(desc(depth))
   if (nrow(coverage) == 0) {
     print(paste("Skipping file (no 'all' rows found):", file))
-    next  # Skip this file
+    next 
   }
   
-  # Calculate cumulative percentage
+  # Calculate cumulative percentage of targetted bases
   coverage$cumulative_percentage <- (cumsum(coverage$proportion_targeted_bases) * 100)
   
   # Calculate average read depth
@@ -39,11 +41,11 @@ for (file in bed_files) {
     Coverage_10x = get_value(coverage, 10, cumulative_percentage),
     Coverage_30x = get_value(coverage, 30, cumulative_percentage),
     Coverage_100x = get_value(coverage, 100, cumulative_percentage),
-    Average_Read_Depth = avg_read_depth  # Added metric
+    Average_Read_Depth = avg_read_depth  
   )
   
-  # Save summary statistics to a CSV file
+  # Save these summary statistics to a CSV file
   write.csv(summary_stats, paste0("summary_", file, ".csv"), row.names = FALSE)
   
   print(paste("Processed:", file))
-}
+} #end of loop
