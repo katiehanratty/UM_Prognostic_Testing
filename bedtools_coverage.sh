7349bf297f063761f2e89b56ea46002f6815083d
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-20 14:30:49 +0100

    Update bedtools_coverage.sh

--- bedtools_coverage.sh
+++ bedtools_coverage.sh
@@ -1,1 +1,7 @@
 #MAKING THE FILES FOR BEDTOOLS COVERAGE TO ASSESS BAM
+# making a histogram of the BAM file then filtering column one to just rows with 'all'
+
+for i in *.bam; do
+bedtools coverage -hist -a ~/CNVkit/bedfile/Exome-Agilent_V6.bed -b $i > ${i%.bam}_depth.txt
+awk '$1=="all"{print $0}' ${i%.bam}_depth.txt > ${i%.bam}_depth_all.txt
+done
