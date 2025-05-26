6751969607f15210f0a6a302f07d0131f6fc42e6
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-26 18:50:23 +0100

    Update CNVkit.sh

--- CNVkit.sh
+++ CNVkit.sh
@@ -2,44 +2,33 @@
 
 #RUNNING CNVKIT
 
-#download bedfile for the specific exome kit used from company website
+#make directories for your target bedfile and your bam cohorts
+mkdir -p ~/CNVkit/bedfiles
+mkdir -p ~/CNVkit/150bp
+mkdir -p ~/CNVkit/151bp
+mkdir ~/alignment/bamfiles/filtered_bam/150bp/matched
+mkdir ~/alignment/bamfiles/filtered_bam/150bp/unmatched
+
+#same pathways into memory, edit as needed depending on users directory names
+REFERENCE=~/alignment/reference/hg38.fa
+BEDFILES=~/CNVkit/bedfiles
+BAMFILES=~/alignment/bamfiles/filtered_bam
+MATCHED=~/alignment/bamfiles/filtered_bam/150bp/matched
+UNMATCHED=~/alignment/bamfiles/filtered_bam/150bp/unmatched
+
+#manually download exome kit bedfile from company website to the bedfiles directory 
+#for my files, Agilent v6 Exon Capture kit was used. 
+
 #set working directory to users bamfile directory
+cd ${BAMFILES}/150bp #edit as needed (e.g. for 150bp, 151bp, etc)
 
 #create access bedfile for use in command
-cnvkit.py access ~/alignment/reference/hg38.fa -o ~/CNVkit_analysis/bedfile/access.bed
+cnvkit.py access ${REFERENCE} -o ${BEDFILES}/access.bed
 
 #PHASE ONE: NO NORMALISATION
 
-#MATCHED SAMPLES (all Eurofins, some Novogene)
-
-for tumour in trimmed_*_tumour_sorted_filtered_nodup_RG.bam; do #edit depending on filenames
-#save patient id into computer memory
-patient_id=${tumour#trimmed_}
-patient_id=${patient_id%_tumour_sorted_filtered_nodup_RG.bam}
-normal="trimmed_${patient_id}_normal_sorted_filtered_nodup_RG.bam"
-	if [[ -f $normal ]]; then
-	echo "Processing patient: $patient_id"
-	cnvkit.py batch $tumour --normal $normal \
-	--targets ~/CNVkit_analysis/bedfile/Exome-Agilent_V6.bed \
-	--fasta ~/alignment/reference/hg38.fa \
-	--access ~/CNVkit_analysis/bedfile/access.bed \
-	--output-dir ~/CNVkit_analysis --scatter
-	else
-	echo "No normal match found, skipping: $patient_id"
-	fi
-done
-
-#UNMATCHED SAMPLES (some Novogene)
-#used on tumour samples sequenced with same platform as a cohort with matched samples
-#reuses reference built from normal pooled samples
-
-for tumour in trimmed_*_tumour_sorted_filtered_nodup_RG.bam; do #edit depending on filenames
-	echo "Processing patient: $patient_id"
-	cnvkit.py batch $tumour 
-	-r ~/CNVkit_analysis/Reference.cnn #reference built from previous matched sample analysis
-	-t targets.bed -a antitargets.bed -g access.bed #targets and antitargets also build previously
-	--output-dir ~/CNVkit_analysis --scatter
-	else
-	echo "No normal match found, skipping: $patient_id"
-	fi
-done
+cnvkit.py batch trimmed_*_tumour_sorted_filtered_nodup_RG.bam --normal trimmed_*_normal_sorted_filtered_nodup_RG.bam \
+--targets ${BEDFILES}/Exome-Agilent_V6.bed \
+--fasta ${REFERENCE} \
+--access ${BEDFILES}/access.bed \
+--output-dir ~/CNVkit/150bp --scatter #edit output directory name as needed
