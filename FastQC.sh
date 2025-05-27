87cc883b86ed3664514158e35292ba842603bcf4
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-27 11:45:00 +0100

    Update FastQC.sh

--- FastQC.sh
+++ FastQC.sh
@@ -3,14 +3,15 @@
 #RUNNING FASTQC
 
 # Create a directory for fastqc outputs, edit pathway as needed
-mkdir -p ~/fastq/FASTQC/MultiQC
-# Import raw paired fastq files manually
+mkdir -p ~/fastq/raw_fastq/FASTQC/MultiQC
+
+# Import raw paired fastq files into raw_fastq directory manually
 
 # Run fastqc, and direct the outputs of the command to user's FASTQC subdirectory
-fastqc ~/fastq/*fastq.gz --outdir=~/fastq/FASTQC
+fastqc ~/fastq/raw_fastq/*fastq.gz --outdir=~/fastq/raw_fastq/FASTQC
 
 # Collate all reports using MultiQC for easier review
-multiqc ~/fastq/FASTQC --outdir=~/fastq/FASTQC/MultiQC
+multiqc ~/fastq/raw_fastq/FASTQC --outdir=~/fastq/raw_fastq/FASTQC/MultiQC
 
 
 
