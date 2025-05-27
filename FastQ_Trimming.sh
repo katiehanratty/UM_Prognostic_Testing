034348e84ad0d766c1fa12fbb92d8f9b6af53804
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-27 11:41:09 +0100

    Update FastQ_Trimming.sh

--- FastQ_Trimming.sh
+++ FastQ_Trimming.sh
@@ -2,6 +2,7 @@
 
 #make a directory for the trimmed fastq
 mkdir -p ~/fastq/trimmed_fastq/FastQC/MultiQC
+mkdir -p ~/fastq/trimmed_fastq/logs
 TRIMMED=/mnt/data/GCB2025/katiehanratty/fastq/trimmed_fastq #edit parent directory as required
 
 #set working directory to users raw fastq directory
@@ -14,11 +15,8 @@ cutadapt -j=1 -a AGATCGGAAGAG -A AGATCGGAAGAG \
 --output=${TRIMMED}/trimmed_$i --paired-output=${TRIMMED}/trimmed_${i%_1.fastq.gz}_2.fastq.gz \
 --error-rate=0.1 --times=1 --overlap=3 --action=trim --minimum-length=35 \
 --pair-filter=any --quality-cutoff=20 \
-$i ${i%_1.fastq.gz}_2.fastq.gz > trimmed_${i%_1.fastq.gz}_cutadapt.log
+$i ${i%_1.fastq.gz}_2.fastq.gz > ${TRIMMED}/logs/trimmed_${i%_1.fastq.gz}_cutadapt.log
 done
-
-#move trimmed files to their own directory 
-mv *.log ${TRIMMED}
 
 #run fastqc and multiqc on the trimmed files
 fastqc ${TRIMMED}/*.fastq.gz --outdir=${TRIMMED}/FastQC
