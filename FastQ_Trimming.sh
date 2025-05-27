29ee4ccb4e72f2d1fbeade35f52ac83652c542a6
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-27 11:39:20 +0100

    Update FastQ_Trimming.sh

--- FastQ_Trimming.sh
+++ FastQ_Trimming.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-#set working directory to users fastq directory
-cd ~/fastq
+#make a directory for the trimmed fastq
+mkdir -p ~/fastq/trimmed_fastq/FastQC/MultiQC
+TRIMMED=/mnt/data/GCB2025/katiehanratty/fastq/trimmed_fastq #edit parent directory as required
+
+#set working directory to users raw fastq directory
+cd ~/fastq/raw_fastq
 
 #run cutadapt 
+#change sequence (-a, -A) as needed (This is Illumina adapter) and other options as required
 for i in *_1.fastq.gz; do
-cutadapt -j=1 -a AGATCGGAAGAG -A AGATCGGAAGAG \  #change sequence as needed (This is Illumina adapter)
---output=trimmed_$i --paired-output=trimmed_${i%_1.fastq.gz}_2.fastq.gz \
---error-rate=0.1 --times=1 --overlap=3 --action=trim --minimum-length=35 \ 
---pair-filter=any --quality-cutoff=20 \ 
-$i ${i%_1.fastq.gz}_2.fastq.gz > trimmed_${i%_1.fastq.gz}_cutadapt.log   #adjust other options as required
+cutadapt -j=1 -a AGATCGGAAGAG -A AGATCGGAAGAG \
+--output=${TRIMMED}/trimmed_$i --paired-output=${TRIMMED}/trimmed_${i%_1.fastq.gz}_2.fastq.gz \
+--error-rate=0.1 --times=1 --overlap=3 --action=trim --minimum-length=35 \
+--pair-filter=any --quality-cutoff=20 \
+$i ${i%_1.fastq.gz}_2.fastq.gz > trimmed_${i%_1.fastq.gz}_cutadapt.log
 done
 
 #move trimmed files to their own directory 
-mkdir -p trimmed_fastq/FastQC/MultiQC
-mv trimmed*fastq.gz trimmed_fastq
+mv *.log ${TRIMMED}
 
 #run fastqc and multiqc on the trimmed files
-cd trimmed_fastq
-fastqc *.fastq.gz --outdir=FastQC
-multiqc trimmed_fastq/FastQC --outdir=./trimmed_fastq/FastQC/MultiQC
+fastqc ${TRIMMED}/*.fastq.gz --outdir=${TRIMMED}/FastQC
+multiqc ${TRIMMED}/FastQC --outdir=${TRIMMED}/FastQC/MultiQC
