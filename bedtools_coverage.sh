3eba37aae6616597341feb02d606d26d758434e7
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-27 13:15:02 +0100

    Update bedtools_coverage.sh

--- bedtools_coverage.sh
+++ bedtools_coverage.sh
@@ -1,7 +1,21 @@
 #MAKING THE FILES FOR BEDTOOLS COVERAGE TO ASSESS BAM
 # making a histogram of the BAM file then filtering column one to just rows with 'all'
 
+#download the target exome capture kit bedfile into a directory, in order to calculate the 
+#level of coverage in the target regions (i.e. the exonic regions).
+#e.g: Agilent v6 https://github.com/AstraZeneca-NGS/reference_data/blob/master/hg38/bed/Exome-Agilent_V6.bed 
+
+#make a directory for the target bedfile and bedtools coverage analysis files
+#edit as required
+mkdir -p ~/alignment/bedtools/bedfile
+BEDTOOLS=/mnt/data/GCB2025/katiehanratty/alignment/bedtools
+
+#move into directory with filtered bamfiles
+cd ~/alignment/bamfiles/filtered_bam || exit 1 #edit as required
+
+#create loop for bedtools coverage
+
 for i in *.bam; do
-bedtools coverage -hist -a ~/CNVkit/bedfile/Exome-Agilent_V6.bed -b $i > ${i%.bam}_depth.txt
-awk '$1=="all"{print $0}' ${i%.bam}_depth.txt > ${i%.bam}_depth_all.txt
+bedtools coverage -hist -a $BEDTOOLS/bedfile/Exome-Agilent_V6.bed -b $i > $BEDTOOLS/${i%.bam}_depth.txt
+awk '$1=="all"{print $0}' $BEDTOOLS/${i%.bam}_depth.txt > $BEDTOOLS/${i%.bam}_depth_all.txt
 done
