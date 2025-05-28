93674aea25a8fb7feb91fe4e4705f7d0be807909
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-05-28 12:15:26 +0100

    Update VEP.sh

--- VEP.sh
+++ VEP.sh
@@ -6,10 +6,13 @@
 #change file locations as required
 cd ~/mutect2
 
+#make a directory for VEP annotations of VCF file
 mkdir ~/mutect2/VEP
 
-#download ensembl VEP cache to parent directory 
+#download ensembl VEP cache to parent directory (note-large file!)
+wget https://ftp.ensembl.org/pub/current_variation/indexed_vep_cache/homo_sapiens_vep_114_GRCh38.tar.gz 
 
+#create loop for vep
 for i in *.vcf; do 
 vep --cache -i $i -fa ~/alignment/reference/hg38.fa \
 --verbose --dir_cache ~/cache \
