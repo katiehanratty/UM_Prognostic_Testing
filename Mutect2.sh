a9ad959aa258a6fb4eb0f7c32316195113343a65
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-06-27 11:56:09 +0100

    Update Mutect2.sh

--- Mutect2.sh
+++ Mutect2.sh
@@ -2,16 +2,14 @@
 
 #Running Mutect2 (Somaitc Variant Calling)
 
-#set working directory to users bamfile directory
-cd ~/alignment/bamfiles/filtered_bam/150bp
+#MAKE SURE YOU ARE IN THE DIRECTORY OF THE BAM FILES YOU WANT TO VARIANT CALL
 
 #make a new directory for your renamed BAM files (Mutect2 only works if files renamed to the sample name used in the BAM reads)
 mkdir -p ~/alignment/bamfiles/filtered_bam/150bp/mutect2
 
 #rename files using mv
 cp *.bam mutect2
-cd mutect2 || exit 1
-for i in *.bam; do
+for i in ~/alignment/bamfiles/filtered_bam/150bp/mutect2/*.bam; do
 mv $i ${i%_sorted_filtered_nodup_RG.bam} 
 done
 
@@ -21,8 +19,7 @@ mkdir -p ~/mutect2/normals
 #CREATE PANEL OF NORMALS
 
 #first run every normal sample through tumor-only Mutect2 (to create Panel of Normals)
-cd ~/alignment/bamfiles/filtered_bam/150bp/mutect2
-for i in trimmed_patient*_normal; do 
+for i in ~/alignment/bamfiles/filtered_bam/150bp/mutect2/trimmed_patient*_normal; do 
 gatk Mutect2 -R ~/alignment/reference/hg38.fa -I $i -tumor $i \
 -O ~/mutect2/normals/${i%.bam}.vcf.gz
 done 
@@ -36,7 +33,7 @@ gatk CreateSomaticPanelOfNormals --vcfs  ~/mutect2/normals/normals_for_pon_vcf.a
 
 #RUN SOMATIC VARIANT CALLING
 
-for tumour in trimmed_*_tumour; do
+for tumour in ~/alignment/bamfiles/filtered_bam/150bp/mutect2/trimmed_*_tumour; do
 #save patient id in computer memory
 patient_id=${tumour#trimmed_}
 patient_id=${patient_id%_tumour}
@@ -52,7 +49,6 @@ normal="trimmed_${patient_id}_normal"
 	fi
 done
 
-cd ~/mutect2
-for i in *.vcf.gz; do
-gatk FilterMutectCalls -V $i -O ${i%.vcf.gz}.filtered.vcf.gz
+for i in ~/mutect2/*.vcf.gz; do
+gatk FilterMutectCalls -V $i -O ~/mutect2/${i%.vcf.gz}.filtered.vcf.gz
 done
